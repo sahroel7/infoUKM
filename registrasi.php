8da9ed8d4f5f8d7bc2d0215f4520f9eb0acb2b24
Sahroell <sahrulbaihaqiasidiq7@gmail.com>
2025-11-18 14:36:32 +0700

    perubahan

--- registrasi.php
+++ registrasi.php
@@ -4,7 +4,6 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 require_once 'koneksi.php';
 
-// Jika sudah login, redirect ke index.php
 if (isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
